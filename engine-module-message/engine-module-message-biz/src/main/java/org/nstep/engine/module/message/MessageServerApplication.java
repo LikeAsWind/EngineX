261f239f0fb30d84a49605961fd7ee56834fa416
yangzhitong <569353464@qq.com>
2024-12-27 10:36:37 +0800

    feat: init-message-server-code

---
+++ engine-module-message/engine-module-message-biz/src/main/java/org/nstep/engine/module/message/MessageServerApplication.java
@@ -0,0 +1,16 @@
+package org.nstep.engine.module.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ */
+@SpringBootApplication
+public class MessageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServerApplication.class, args);
+    }
+
+}
